f8515fb103fcb4e0608748e4cd19d233cc236e51
Shahwar Saleem <shahwarleo@gmail.com>
2016-10-06 15:12:21 -0400

    functional code commit with timing wrt assignment document

--- producer_consumer/processes/example.c
+++ producer_consumer/processes/example.c
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+#define MAX_NUMBER 2000
+int N, B;
 
 
 void die(char *s)
@@ -19,20 +23,86 @@ struct msg
     int     mesg;
 };
 
-int main(int argc, char *argv[])
+void consumer(void)
+{
+    struct msg rcvbuffer;
+    int msqid; 
+    key_t key = 1234;
+    if ((msqid = msgget(key, 0666)) < 0)
+        die("msgget()");
+    
+    int num;
+    int i = 0;
+    while (i < N) 
+    {
+        //Receive an answer of message type 1.
+        if (msgrcv(msqid, &rcvbuffer, sizeof(int), 1, 0) < 0)
+            die("msgrcv");
+        
+        printf("Consumed : %d\n", rcvbuffer.mesg);
+        num = rcvbuffer.mesg;
+        i++;
+    }
+    printf("received %d messages\n", i);
+}
+
+
+void producer(void)
 {
     int msqid;
     int msgflg = IPC_CREAT | 0666;
     key_t key;
     struct msg msg_buf;
     struct msqid_ds msqid_buf;
-    int N,B;
 
+    /* Arbitrary value for key to initialize the message queue */ 
     key = 1234;
 
-    pid_t pid;
+    if ((msqid = msgget(key, msgflg )) < 0)   //Get the message queue ID for the given key
+           die("msgget");
 
+        if (msgctl(msqid, IPC_STAT, &msqid_buf) == -1)
+        {
+            perror("msgctl: msgctl failed");
+            exit(1);
+        }
+
+        msqid_buf.msg_qbytes =  B*sizeof(int);
+
+        if (msgctl(msqid, IPC_SET, &msqid_buf) == -1) 
+        {
+            perror("msgctl: msgctl failed");
+            exit(1);
+        }
+       
+        //Message Type
+        msg_buf.m_type = 1;
+
+        int i = 0;
+        while ( i < N)
+        {
+            msg_buf.mesg = rand() % MAX_NUMBER;
+
+            while(msgsnd(msqid, &msg_buf, sizeof(int), IPC_NOWAIT) < 0);
+            printf("Produced: %d\n", msg_buf.mesg);
+            i++;
+        }
+     //   wait(NULL);
+        printf("Transmitted %d messages\n", i);
+        wait(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    pid_t pid;
     srand(time(NULL));
+
+    struct timeval start_t, init_t, end_t;
+    long double interval_usec = 0.0;
+    long double initialzation_t_usec, transmission_t_usec, init_t_usec, start_t_usec, end_t_usec;
+
+   /* Start measuring time for initialization */
+    gettimeofday(&start_t, NULL);
 
     if ( argc != 3) 
     {
@@ -55,6 +125,9 @@ int main(int argc, char *argv[])
     N = atoi(argv[1]);
     B = atoi(argv[2]);
 
+    /* Check point for initialization time measurement */
+    gettimeofday(&init_t, NULL);
+
     pid = fork();
 
     if (pid<0)
@@ -63,63 +136,32 @@ int main(int argc, char *argv[])
     }
     else if (pid == 0)
     {
-       // execlp("./consumer", NULL);
-      struct msg rcvbuffer;
-      if ((msqid = msgget(key, 0666)) < 0)
-      die("msgget()");
+        /*Child process should call the consumer.*/
+        consumer();
+        /* checkpoint for threads ending */
+        gettimeofday(&end_t, NULL);
 
-      int i = 0;
-      while (i < N) 
-      {
-         //Receive an answer of message type 1.
-         if (msgrcv(msqid, &rcvbuffer, sizeof(int), 1, 0) < 0)
-            die("msgrcv");
+        /* timing extraction from the data structure */
+        start_t_usec = (long double) (start_t.tv_sec * 1000000 + start_t.tv_usec);
+        init_t_usec = (long double) (init_t.tv_sec * 1000000 + init_t.tv_usec);
 
-         printf("%d\n", rcvbuffer.mesg);
-         i++;
-      }
-     
+        /* Keep the initialization time as a difference */
+        initialzation_t_usec = init_t_usec - start_t_usec;
+
+        init_t_usec = (long double) (init_t.tv_sec * 1000000 + init_t.tv_usec);
+        end_t_usec = (long double) (end_t.tv_sec * 1000000 + end_t.tv_usec);
+
+        transmission_t_usec = end_t_usec - init_t_usec;
+
+        printf("Initialization time = %Lf in seconds %d\n", (initialzation_t_usec / 1000000), pid);
+        printf("Time to transfer data = %Lf seconds %d \n", (transmission_t_usec / 1000000), pid);
     }
     else
     {   
-        if ((msqid = msgget(key, msgflg )) < 0)   //Get the message queue ID for the given key
-           die("msgget");
-
-        if (msgctl(msqid, IPC_STAT, &msqid_buf) == -1)
-        {
-            perror("msgctl: msgctl failed");
-            exit(1);
-        }
-
-        msqid_buf.msg_qbytes =  B;
-
-        if (msgctl(msqid, IPC_SET, &msqid_buf) == -1) 
-        {
-            perror("msgctl: msgctl failed");
-            exit(1);
-        }
-        //Message Type
-        printf("In parent");
-        msg_buf.m_type = 1;
-
-        int i = 0;
-        while ( i < N)
-        {
-           
-            msg_buf.mesg = rand();
-            if (msgsnd(msqid, &msg_buf, sizeof(int), IPC_NOWAIT) < 0)
-            {
-                 printf ("%d, %d, %d, %d\n", msqid, msg_buf.m_type, msg_buf.mesg, sizeof(int));
-                 die("msgsnd");
-            }
-            else
-            {
-                 i++;
-            }
-        }
-     }
-    
-
+        /* Parent process should call the producer.*/
+        producer();
+    }
+    wait(NULL);
     exit(0);
 }
 
--- producer_consumer/threads/producer_consumer.c
+++ producer_consumer/threads/producer_consumer.c
@@ -4,96 +4,123 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-
-void *producer(void *params);
-void *consumer(void *params);
-
 int *buffer;
 int N, B;
 
+/* Variabled to implement bounded buffer. */
 int front = 0;
 int  end  = 0;
+
+/* Mutex lock for thread synchronization within critical sections. */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* Function to start producer thread*/
+void *producer(void  *params)
+{
+    srand(time(NULL));
+    int i = 0;
+    while ( i<N )
+    {
+        while(((front+1) % B) == end);
+
+        buffer[front] = rand();
+
+        pthread_mutex_lock(&mutex);
+        front = (front + 1) % B;
+        pthread_mutex_unlock(&mutex);
+
+        i++;
+    }
+}
+
+/* Function to start comsumer */
+void *consumer(void *params)
+{
+    int i = 0;
+    int num = 0;
+    while ( i<N )
+    {
+        while (front == end);
+
+        num = buffer[end];
+        //printf("\n%d\n", buffer[end]);
+        pthread_mutex_lock(&mutex);
+
+        end = (end + 1) % B;
+        pthread_mutex_unlock(&mutex);
+
+        i++;
+    }
+}
 
 int main(int argc, char *argv[])
 {
 	pthread_t producer_id, consumer_id;
     pthread_attr_t attr;
 
-    struct timeval start_t, end_t;
+    struct timeval start_t, init_t, end_t;
     long double interval_usec = 0.0;
-    long double start_t_usec, end_t_usec;
+    long double initialzation_t_usec, transmission_t_usec, init_t_usec, start_t_usec, end_t_usec;
 
+    /* Start measuring time for initialization */
+    gettimeofday(&start_t, NULL);
+
+    /* Error check if there are more or less arguments than required*/
     if ( argc != 3) 
     {
         fprintf(stderr, "usage: producer_consumer <Number of integers> <Buffer Size>");
         return -1;
     }
 
+    /* Error check if one of the arguments is negative.*/
     if ( atoi(argv[1]) < 0 || atoi(argv[2]) < 0) 
     { 
         fprintf(stderr, "Both arguments must be positive integers\n");
         return -1;
     }
 
+    /* Check if the arguments satisfy the condition N > B*/
     if ( atoi(argv[1]) < atoi(argv[2]) ) 
     {
         fprintf(stderr, "Number of integers must be greater than buffer size.\n");
         return -1;
     }
 
-
+    /* Extract the arguments to integers.*/
     N = atoi(argv[1]);
     B = atoi(argv[2]);
 
+    /* Buffer allocation for shared memory between threads */
 	buffer = malloc(B * sizeof(int));
 
+    /* Thread attribute initialization */
     pthread_attr_init(&attr);
 
-    // Creating a thread here
+    /* Creating consumer and producer threads here*/
     pthread_create(&producer_id, &attr, producer, argv[1]);
     pthread_create(&consumer_id, &attr, consumer, argv[1]);
 
-    gettimeofday(&start_t, NULL);
+    /* Check point for initialization time measurement */
+    gettimeofday(&init_t, NULL);
+
+    /* timing extraction from the data structure */
+    start_t_usec = (long double) (start_t.tv_sec * 1000000 + start_t.tv_usec);
+    init_t_usec = (long double) (init_t.tv_sec * 1000000 + init_t.tv_usec);
+
+    /* Keep the initialization time as a difference */
+    initialzation_t_usec = init_t_usec - start_t_usec;
     pthread_join(producer_id, NULL);
     pthread_join(consumer_id, NULL);
+
+    /* checkpoint for threads ending */
     gettimeofday(&end_t, NULL);
 
+    init_t_usec = (long double) (init_t.tv_sec * 1000000 + init_t.tv_usec);
+    end_t_usec = (long double) (end_t.tv_sec * 1000000 + end_t.tv_usec);
 
-   start_t_usec = (long double) (start_t.tv_sec * 1000000 + start_t.tv_usec);
-   end_t_usec = (long double) (end_t.tv_sec * 1000000 + end_t.tv_usec);
+    transmission_t_usec = end_t_usec - init_t_usec;
 
-   interval_usec = end_t_usec - start_t_usec;
+    printf("Initialization time = %Lf in seconds\n", (initialzation_t_usec / 1000000));
+    printf("Time to transfer data = %Lf seconds\n", (transmission_t_usec / 1000000));
 
-    printf("Initialization time = %Lf in seconds\n", (start_t_usec / 1000000));
-    printf("Time to transfer data = %Lf seconds\n", (interval_usec / 1000000));
-
-}
-
-void *producer(void  *params)
-{
-	srand(time(NULL));
-	int i = 0;
-    while ( i<N )
-    {
-        while(((front+1) % B) == end);
-
-        buffer[front] = rand() ;
-        front = (front + 1) % B;
-        i++;
-    }
-}
-
-void *consumer(void *params)
-{
-	int i = 0;
-	int num = 0;
-	while ( i<N )
-	{
-		while (front == end);
-
-        num = buffer[end];
-		printf("\n%d\n", buffer[end]);
-		end = (end + 1) % B;
-		i++;
-	}
 }
